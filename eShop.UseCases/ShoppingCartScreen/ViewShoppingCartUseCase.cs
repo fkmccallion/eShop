6581910148c234fed078ac3c8d7031c0eb759e5c
fkmccallion <fkmccallion@gmail.com>
2021-10-04 06:42:06 -0700

    View Shopping Cart Use Case

---
+++ eShop.UseCases/ShoppingCartScreen/ViewShoppingCartUseCase.cs
@@ -0,0 +1,25 @@
+﻿using eShop.CoreBusiness.Models;
+using eShop.UseCases.PluginInterfaces.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShop.UseCases.ShoppingCartScreen
+{
+    public class ViewShoppingCartUseCase
+    {
+        private readonly IShoppingCart shoppingCart;
+
+        public ViewShoppingCartUseCase(IShoppingCart shoppingCart)
+        {
+            this.shoppingCart = shoppingCart;
+        }
+
+        public Task<Order> Execute()
+        {
+            return shoppingCart.GetOrderAsync();
+        }
+    }
+}
