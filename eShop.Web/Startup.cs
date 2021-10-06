77f24fc6bae9dbd9824267ff5d7f26a010271365
fkmccallion <fkmccallion@gmail.com>
2021-10-06 05:39:19 -0700

    Delete and Update with Order Line Component

--- eShop.Web/Startup.cs
+++ eShop.Web/Startup.cs
@@ -47,6 +47,8 @@ namespace eShop.Web
             services.AddTransient<ISearchProductUseCase, SearchProductUseCase>();
             services.AddTransient<IAddProductToCartUseCase, AddProductToCartUseCase>();
             services.AddTransient<IViewShoppingCartUseCase, ViewShoppingCartUseCase>();
+            services.AddTransient<IDeleteProductUseCase, DeleteProductUseCase>();
+            services.AddTransient<IUpdateQuantityUseCase, UpdateQuantityUseCase>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
