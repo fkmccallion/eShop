8fa99ea89f4ae8e63d618b6b0fad2aac74ada772
Keith McCallion <kmccallion@wilwood.com>
2021-10-07 16:06:57 -0700

    Authenticate End Point

---
+++ eShop.Web/Controllers/AuthenticationController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace eShop.Web.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        [Route("/authenticate")]
+        public async Task<IActionResult> Authenticate([FromQuery]string user, [FromQuery]string pwd)
+        {
+            if (user == "admin" && pwd == "adminadmin")
+            {
+                var userClaims = new List<Claim>()
+                {
+                    new Claim(ClaimTypes.Name, user),
+                    new Claim(ClaimTypes.Email, "admin@eshop.com"),
+                    new Claim(ClaimTypes.HomePhone, "12345678")
+                };
+
+                var userIdentity = new ClaimsIdentity(userClaims, "eShop.CookieAuth");
+                var userPrincipal = new ClaimsPrincipal(userIdentity);
+
+                await HttpContext.SignInAsync("eShop.CookieAuth", userPrincipal);
+            }
+
+            return Redirect("/outstandingorders");
+        }
+
+        [Route("/logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return Redirect("/outstandingorders");
+        }
+    }
+}
